48c4e756ad1ac871eeaa74a71205c0dca063229a
Federico Romero <federomeroo@hotmail.com>
2023-09-02 02:06:10 -0300

    Tercera entrega

--- main.js
+++ main.js
@@ -1,97 +1,94 @@
-alert("Bienvenidos a LibreMercado"); 
-
-class Procesadores { 
-    constructor(marca, modelo, precio) {
-        this.marca = marca;
-        this.modelo = modelo;
+class Producto {
+    constructor(nombre, precio, imagen) {
+        this.nombre = nombre;
         this.precio = precio;
+        this.imagen = imagen;
     }
-};
-
-const Productos = () => {
-    const productosCargados = [];
-    productosCargados.push(new Procesadores("AMD", "Ryzen 3", 81000));
-    productosCargados.push(new Procesadores("AMD", "Ryzen 5", 178000));
-    productosCargados.push(new Procesadores("AMD", "Ryzen 7", 252800));
-    productosCargados.push(new Procesadores("Intel", "I3", 79000));
-    productosCargados.push(new Procesadores("Intel", "I5", 146000));
-    productosCargados.push(new Procesadores("Intel", "I7", 210400));
-    return productosCargados;
-};
-
-const compraOVenta = () => { 
-    let venderOComprar = prompt("Quieres vender o comprar procesadores?");
-    if (venderOComprar.toLowerCase() === "comprar") {
-        let Eleccion = Comprar(); 
-        cuponDescuento(Eleccion); 
-    } else if (venderOComprar.toLowerCase() === "vender") {
-        let procesadoresPreCargados = Productos(); 
-        let productosCargadosPorElUsuario = venderProcesador(); 
-        concatDeProcesadores(procesadoresPreCargados, productosCargadosPorElUsuario); 
-    } else alert("Opcion invalida");
-};
-
-const Comprar = () => {
-    let comprar = prompt("Elija marca y modelo del procesador"); 
-    let productosCargados = Productos();
-    let procesadorAComprar = [];
-    const filtro = productosCargados.filter(item => { 
-        return item.marca.toLowerCase() === comprar.toLowerCase();
-    });
-
-    if (filtro.length === 0) { 
-        alert(`No tenemos stock de esa marca ${comprar}`);
-        const marcas = [...new Set(productosCargados.map(procesador => procesador.marca))];
-        let marcasCadena = marcas.join(", "); 
-        let mensaje = `Tenemos procesadores de estas marcas ${marcasCadena}`;
-        alert(mensaje);
-        Comprar();
-    };
-
-    filtro.forEach((item, index) => { 
-        let mensaje = `
-        Procesador: ${index + 1}
-        Marca: ${item.marca}
-        Modelo: ${item.modelo}
-        Precio: $${item.precio}`;
-        procesadorAComprar.push(item);
-        alert(mensaje);
-    });
-
-    let opcion = parseInt(prompt("Ingrese el número del procesador deseado")); 
-    const procesadorElegido = filtro[opcion - 1];
-    if (opcion >= 1 && opcion <= filtro.length) {
-        alert(`Usted eligio ${procesadorElegido.marca}, ${procesadorElegido.modelo} `);
-    } else alert("Opción inválida.");
-    alert("Gracias por comprar tu Procesador con nosotros. A continuación ingrese su email para ponernos en contacto con usted.")
-    prompt("Ingrese su email");
-    alert("Gracias por mandar su email, nos mantendremos en contacto a la brevedad.")
-    return procesadorAComprar;
-};
-
-const venderProcesador = () => {
-    let vender = confirm("Quieres vender tu procesador?");
-    const cargarProcesador = [];
-    while (vender === true) {
-        cargarProcesador.push(new Procesadores(prompt("Ingrese la marca"),
-            prompt("Ingrese el modelo del procesador"),
-            parseInt(prompt("Ingrese el precio al que desea venderlo"))));
-        vender = confirm("Quieres vender otro Procesador?");
-    };
-    return cargarProcesador;
-};
-
-const concatDeProcesadores = (procesadoresPreCargados, productosCargadosPorElUsuario) => {
-    let concatDeProcesadores = procesadoresPreCargados.concat(productosCargadosPorElUsuario);
-    
-    if (productosCargadosPorElUsuario.length > 1) {
-        alert(`Gracias por vender ${productosCargadosPorElUsuario.length} procesadores con nosotros. Ingrese a continuación su email para finalizar su venta.`);
-    } else if (productosCargadosPorElUsuario.length === 1) {
-        alert("Gracias por vender tu procesador con nosotros. Ingrese a continuación su email para finalizar su venta.");
-        prompt("Ingrese su email");
-        alert("Gracias por mandar su email, nos mantendremos en contacto a la brevedad.")
-    } else alert("Nos encantaria que vendas tu Procesador con nosotros.");
-    return concatDeProcesadores;
 }
 
-compraOVenta();
+const productos = [
+    new Producto("Muzzarella", 3800, "./imagenes/mozzarella.jpg"),
+    new Producto("Napolitana", 4000, "./imagenes/napo.webp"),
+    new Producto("Anchoas", 4100, "./imagenes/pizza-anchoas.jpg"),
+    new Producto("Veggie", 4200, "./imagenes/pizza_veggie.jpg"),
+    new Producto("Jamon y huevo", 4000, "./imagenes/pizza-huevoyj.jpeg"),
+    new Producto("Jamon y morrón", 4500, "./imagenes/pizza_jym.jpg"),
+    new Producto("Fugazzeta rellena", 5000, "./imagenes/pizza-fugarellena2.jpg"),
+    new Producto("Tropical", 4500, "./imagenes/pizza-tropical.jpg"),
+    new Producto("Cuatro Quesos", 4900, "./imagenes/pizza-cuatroquesos.jpg"),
+];
+
+const calcularTotal = (cantidad, precio) => cantidad * precio;
+
+const productosContainer = document.querySelector('.productos');
+const carritoLista = document.querySelector('.carrito-lista');
+const totalElement = document.getElementById('total');
+const finalizarCompraButton = document.getElementById('finalizar-compra');
+
+function mostrarProductos() {
+    productosContainer.innerHTML = productos.map((producto, index) => `
+        <div class="producto">
+            <img src="${producto.imagen}" alt="${producto.nombre}">
+            <span>${producto.nombre} - $${producto.precio}</span>
+            <input type="number" class="cantidad" min="1" value="1">
+            <button class="agregar-carrito" data-index="${index}">Agregar al carrito</button>
+        </div>
+    `).join('');
+}
+
+function actualizarCarrito() {
+    const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+    carritoLista.innerHTML = carrito.map((item, index) => `
+        <li>
+            <img src="${item.imagen}" alt="${item.nombre}" class="miniatura">
+            ${item.nombre} - Cantidad: ${item.cantidad} - Subtotal: $${item.subtotal}
+            <button class="eliminar-producto" data-index="${index}">Eliminar</button>
+        </li>
+    `).join('');
+
+    const total = carrito.reduce((acc, item) => acc + item.subtotal, 0);
+    totalElement.textContent = total;
+}
+function agregarAlCarrito(event) {
+    if (event.target.classList.contains('agregar-carrito')) {
+        const index = parseInt(event.target.dataset.index);
+        const cantidad = parseInt(event.target.previousElementSibling.value);
+        const productoSeleccionado = productos[index];
+        const subtotal = calcularTotal(cantidad, productoSeleccionado.precio);
+
+        const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+        carrito.push({
+            nombre: productoSeleccionado.nombre,
+            cantidad: cantidad,
+            subtotal: subtotal,
+            imagen: productoSeleccionado.imagen
+        });
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+
+        actualizarCarrito();
+    }
+}
+
+function eliminarDelCarrito(event) {
+    if (event.target.classList.contains('eliminar-producto')) {
+        const index = parseInt(event.target.dataset.index);
+
+        const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+        carrito.splice(index, 1);
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+
+        actualizarCarrito();
+    }
+}
+
+function finalizarCompra() {
+    localStorage.removeItem("carrito");
+    actualizarCarrito();
+    alert("El envio le llegará en aproximadamente 30 minutos, muchas gracias!");
+}
+
+mostrarProductos();
+actualizarCarrito();
+productosContainer.addEventListener('click', agregarAlCarrito);
+carritoLista.addEventListener('click', eliminarDelCarrito);
+finalizarCompraButton.addEventListener('click', finalizarCompra);
